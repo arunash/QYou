3880755df6385cdadf59afa2986362d4ca2086b5
Pete Santhappan <p_santhappan@hotmail.com>
2015-03-07 10:13:38 -0800

    Initial Commit

---
+++ Q You/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Q You
+//
+//  Created by Prasanna Santhappan on 3/7/15.
+//  Copyright (c) 2015 Prasanna Santhappan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Q You/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Q You
+//
+//  Created by Prasanna Santhappan on 3/7/15.
+//  Copyright (c) 2015 Prasanna Santhappan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
